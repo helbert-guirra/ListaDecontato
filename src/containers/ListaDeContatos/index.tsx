fb5aa29283d0f37ceca0913cd980ea848c599e90
HelbertGuirra <helbert.guirra@gmail.com>
2025-07-17 20:44:39 -0300

    commit inicial

---
+++ src/containers/ListaDeContatos/index.tsx
@@ -0,0 +1,14 @@
+// containers/ListaDeContatos.js
+import React from 'react'
+
+const ListaDeContatos = () => {
+  return (
+    <main>
+      <p>
+        2 contatos marcados como: &quot;categoria&ldquo; e &quot;termo&ldquo;
+      </p>
+    </main>
+  )
+}
+
+export default ListaDeContatos
